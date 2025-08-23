2c8c61920b917bbc380661f95d88259790ac781e
kimis <kggs0719@naver.com>
2025-08-23 12:04:49 +0900

    게시글번호, 조회수 컬럼생성

--- models/Post.ts
+++ models/Post.ts
@@ -3,19 +3,23 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 // TypeScript에서 사용할 Post 문서의 인터페이스
 export interface IPost extends Document {
+    postId: string;
     title: string;
     content: string;
     authorEmail: string;
     like: Number;
+    viewCount: Number;
 }
 
 // Mongoose 스키마 정의
 const PostSchema: Schema = new Schema(
     {
+        postId: { type: String, required: true, unique: true },
         title: { type: String, required: true },
         content: { type: String, required: true },
         authorEmail: { type: String, required: true },
         like: { type: Number, required: false },
+        viewCount: { type: Number, required: false },
     },
     {
         timestamps: true, // createdAt, updatedAt 자동 기록
